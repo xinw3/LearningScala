59c8cc762bfd08a7d1915da68525b9216fcb54f2
Bella <xinw3@andrew.cmu.edu>
2018-03-12 16:50:28 -0500

    groupby and aggregate functions

--- spark-Jose/Scala-and-Spark-Bootcamp-master/Spark DataFrames/df.scala
+++ spark-Jose/Scala-and-Spark-Bootcamp-master/Spark DataFrames/df.scala
@@ -4,27 +4,11 @@ val spark = SparkSession.builder().getOrCreate()
 
 // create a DataFrame as a SparkSession
 // DataFrame will treat every field as a string
-val df = spark.read.option("header", "true").option("inferSchema", "true").csv("CitiGroup2006_2008")
-df.printSchema()
+val df = spark.read.option("header", "true").option("inferSchema", "true").csv("Sales.csv")
 
-// two main ways to filter out the data
-// one way is to use spark sql syntax, no need to import anything
-// the other way is to use scala syntax, $, need to import
+// df.groupBy("Company").sum().show()
+// df.select(sum("Sales")).show()
+// df.select(sumDistinct("Sales")).show()
 
-import spark.implicits._
-// Scala notation
-// df.filter($"Close" < 480 && $"High" < 480).show()
-
-// sql notation
-// collect() is the way to transform dataframe to Array objects
-// val ch_low = df.filter("Close < 480 AND High < 480").collect()
-
-// val ch_low = df.filter("Close < 480 AND High < 480").count()
-
-// triple equal signs for determining equal
-// df.filter($"High" === 484.40).show()
-
-// or use sql notation
-// df.filter("High = 484.40").show()
-
-df.select(corr("High", "Low")).show()
+df.show()
+df.orderBy($"Sales".desc).show()
