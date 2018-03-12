33d0f0efa9c73a8826e4f0f1cad5ba943697e6a9
Bella <xinw3@andrew.cmu.edu>
2018-03-12 14:56:10 -0500

    dataframe basic functions

--- spark-Jose/Scala-and-Spark-Bootcamp-master/Spark DataFrames/df.scala
+++ spark-Jose/Scala-and-Spark-Bootcamp-master/Spark DataFrames/df.scala
@@ -1,5 +1,17 @@
 import org.apache.spark.sql.SparkSession
 
-val = SparkSession
-    .builder()
-    .getOrCreate()
+val spark = SparkSession.builder().getOrCreate()
+
+// create a DataFrame as a SparkSession
+// DataFrame will treat every field as a string
+val df = spark.read.option("header", "true").option("inferSchema", "true").csv("CitiGroup2006_2008")
+
+// df.head(5)
+
+// compute the statistics for numeric and string columns
+// df.describe().show()
+
+val df2 = df.withColumn("HighPlusLow", df("High") + df("Low"))
+df2.select(df2("HighPlusLow").as("HPL"), df2("Close")).show
+
+df2.printSchema()
