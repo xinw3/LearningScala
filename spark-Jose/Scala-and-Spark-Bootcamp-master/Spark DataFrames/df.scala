7ec1547544c36eb8c9542653fab85dd99e5a5b0d
Bella <xinw3@andrew.cmu.edu>
2018-03-13 09:57:05 -0500

    date and timestamps info

--- spark-Jose/Scala-and-Spark-Bootcamp-master/Spark DataFrames/df.scala
+++ spark-Jose/Scala-and-Spark-Bootcamp-master/Spark DataFrames/df.scala
@@ -4,18 +4,16 @@ val spark = SparkSession.builder().getOrCreate()
 
 // create a DataFrame as a SparkSession
 // DataFrame will treat every field as a string
-val df = spark.read.option("header", "true").option("inferSchema", "true").csv("ContainsNull.csv")
+val df = spark.read.option("header", "true").option("inferSchema", "true").csv("CitiGroup2006_2008")
+// Spark has lots of APIs dealing with dates and timestamps info
 
-df.printSchema()
+// Return a new dataset by adding a column or
+// replacing the existing column that has the same name
+val df2 = df.withColumn("Year", year(df("Date")))
+// df2.show()
 
-df.show()
+// val dfavgs = df2.groupBy("Year").mean()
+// dfavgs.show()
 
-// Look through the columns, find the same datatype
-// and fill in null values with the value we set
-// df.na.fill(100).show()
-
-// df.na.fill("Missing Name").show()
-
-// Instead of fill in any null columns with the same datatype,
-// we could specify the column names as an array and only fill in these columns
-df.na.fill("New Name", Array("Name")).show()
+val dfmins = df2.groupBy("Year").min()
+dfmins.show()
