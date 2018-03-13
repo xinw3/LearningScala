daa284bfd1398b85e6e92cb74883954f8199931f
Bella <xinw3@andrew.cmu.edu>
2018-03-13 09:36:43 -0500

    missing data

--- spark-Jose/Scala-and-Spark-Bootcamp-master/Spark DataFrames/df.scala
+++ spark-Jose/Scala-and-Spark-Bootcamp-master/Spark DataFrames/df.scala
@@ -4,11 +4,18 @@ val spark = SparkSession.builder().getOrCreate()
 
 // create a DataFrame as a SparkSession
 // DataFrame will treat every field as a string
-val df = spark.read.option("header", "true").option("inferSchema", "true").csv("Sales.csv")
+val df = spark.read.option("header", "true").option("inferSchema", "true").csv("ContainsNull.csv")
 
-// df.groupBy("Company").sum().show()
-// df.select(sum("Sales")).show()
-// df.select(sumDistinct("Sales")).show()
+df.printSchema()
 
 df.show()
-df.orderBy($"Sales".desc).show()
+
+// Look through the columns, find the same datatype
+// and fill in null values with the value we set
+// df.na.fill(100).show()
+
+// df.na.fill("Missing Name").show()
+
+// Instead of fill in any null columns with the same datatype,
+// we could specify the column names as an array and only fill in these columns
+df.na.fill("New Name", Array("Name")).show()
